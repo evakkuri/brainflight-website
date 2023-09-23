4a1455899d19adc358660d7ba0c971a0c7c6c88d
Elias Vakkuri <elias.vakkuri@brainflight.fi>
2023-09-23 14:47:42 +0300

    feat: Updates to project section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,11 +3,11 @@ import type Project from '../types/Project';
 export const projects: Project[] = [
   {
     industry: 'Energy',
-    title: 'Full Stack Development',
+    title: 'Open Data Portal',
     startTime: new Date('2022-10-01'),
     client: 'Energy company',
     responsibilities:
-      "Creating the next version of the company's Open Data portal, including architecture, databases, API's, and front-end.",
+      "Full-stack development of the company's Open Data portal. Key responsiblities: architecture design, full-stack development for frontend, API, database.",
     keyWords: ['Azure', 'TypeScript', 'Next.js', 'Express', 'Azure SQL'],
     imageLineAwesomeClass: 'la-bolt',
   },
@@ -17,18 +17,41 @@ export const projects: Project[] = [
     startTime: new Date('2023-05-01'),
     client: 'Healthcare consultancy',
     responsibilities:
-      'Assisting the client in setting up an Azure & Snowflake data platform. Key responsibilities include architecture & network planning, cloud resource setup, and data engineering.',
+      'Assisting the client in setting up an Azure & Snowflake data platform. Key responsibilities: architecture & network planning, cloud resource setup, and data engineering.',
     keyWords: ['Azure', 'Snowflake', 'Terraform', 'Data Engineering'],
     imageLineAwesomeClass: 'la-laptop-medical',
   },
   {
     industry: 'Retail',
-    title: 'AWS Data Engineering',
+    title: 'AWS data platform setup',
     startTime: new Date('2021-09-01'),
     endTime: new Date('2023-05-01'),
     client: 'Large Finnish retailer',
     responsibilities:
-      "Data Engineer in a team modernizing data warehousing and reporting for a Finnish retailer's wholesale division. A lot of cloud engineering and Lambda development for integrations and Snowflake external functions.",
+      "Part of team building a modern data warehouse for a Finnish retailer's wholesale division. Key responsibilities: cloud engineering and Lambda development for integrations and Snowflake external functions.",
     keyWords: ['AWS', 'Terraform', 'Snowflake', 'Agile Data Engine'],
+    imageLineAwesomeClass: 'la-boxes',
+  },
+  {
+    industry: 'IT Services',
+    title: 'Banking service development',
+    startTime: new Date('2021-09-01'),
+    endTime: new Date('2022-10-01'),
+    client: 'IT Services Provider',
+    responsibilities:
+      "Part of client's team developing new machine learning-based services for banking. Key responsibilites: machine learning engineering, API development, improving development practices.",
+    keyWords: ['Azure', 'Azure Machine Learning', 'Databricks', 'Python', 'Terraform'],
+    imageLineAwesomeClass: 'la-university',
+  },
+  {
+    industry: 'Technology',
+    title: 'Improving efficiency of machine learning solution',
+    startTime: new Date('2022-04-01'),
+    endTime: new Date('2022-04-01'),
+    client: 'Technology Startup',
+    responsibilities:
+      'Worked with client to improve the runtime efficiency of their machine learning solution. Key responsibilities: architecture plannning, refactoring, performance testing.',
+    keyWords: ['GCP', 'Python', 'Docker', 'Natural Language Processing'],
+    imageLineAwesomeClass: 'la-laptop-code',
   },
 ];
