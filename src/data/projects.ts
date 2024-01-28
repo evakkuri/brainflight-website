c059f2b2d40d56bc41b2b9752d3f8f7bb6e4605a
Elias Vakkuri <elias.vakkuri@brainflight.fi>
2024-01-28 19:32:05 +0200

    Further improvements to projects section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,7 +9,7 @@ export const projects: Project[] = [
     client: 'Technology Startup',
     allocation: 0.2,
     description:
-      'Worked with client to improve the runtime efficiency of their machine learning solution. ' +
+      'Worked with client to improve the runtime efficiency of their machine learning solution.\n' +
       'Within a short timeframe we were able to locate and fix a severe bottleneck in the ' +
       'solution, improving response time considerably.',
     responsibilities: ['Machine Learning Engineering'],
@@ -25,10 +25,10 @@ export const projects: Project[] = [
     allocation: 0.2,
     description:
       "Our small team modernized the client's data and reporting platform by migrating existing " +
-      'solution to AWS, Snowflake and Power BI. Project highlights were ' +
-      'close collaboration with client stakeholders and successful delivery of value in small ' +
-      'packages. We received special thanks for the maintainability and high quality of ' +
-      'documentation of our solution.',
+      'solution to AWS, Snowflake and Power BI.\n' +
+      'Project highlights were close collaboration with client stakeholders and successful ' +
+      'delivery of value in small packages. We received special thanks for the maintainability ' +
+      'and high quality of our documentation, making handover very efficient.',
     responsibilities: ['Cloud Engineering', 'Data Engineering'],
     technologies: ['AWS', 'Lambda', 'Python', 'Terraform', 'Snowflake', 'Agile Data Engine'],
     imageLineAwesomeClass: 'la-boxes',
@@ -41,10 +41,11 @@ export const projects: Project[] = [
     client: 'IT Services Provider',
     allocation: 0.8,
     description:
-      "Worked as part of client's team developing new ML-based banking services. My " +
-      "main responsibility was to bring data scientist's models to production by improving code " +
-      'quality, testing and performance. I also worked on the architecture and development of ' +
-      'the backend and data processing, and participated in setting product strategy.',
+      "Worked as part of client's team developing new ML-based banking services.\n" +
+      "My main responsibility was to bring data scientist's models to production by improving " +
+      'code quality, testing and performance.\n' +
+      'I also worked on the architecture and development of the backend and data processing, and ' +
+      'participated in setting product strategy.',
     responsibilities: [
       'Machine Learning Engineering',
       'Architecture',
@@ -62,7 +63,7 @@ export const projects: Project[] = [
     client: 'Energy company',
     allocation: 0.8,
     description:
-      "Developed new version of client's Open Data portal from designs to production. " +
+      "Developed new version of client's Open Data portal from designs to production.\n" +
       'The new version has improved user experience and a more maintanable application ' +
       "architecture as part of client's technology landscape.",
     responsibilities: ['Architecture', 'Full Stack Development'],
@@ -77,9 +78,10 @@ export const projects: Project[] = [
     client: 'Healthcare consultancy',
     allocation: 0.2,
     description:
-      'PoC for setting up an Azure & Snowflake cloud data platform. ' +
+      'PoC for setting up an Azure & Snowflake cloud data platform.\n' +
       "We evaluated if a cloud data platform would be a good alternative to client's existing " +
-      'on-premise solution from security, performance, cost, and developer experience perspectives.',
+      'on-premise solution from security, performance, cost, and developer experience ' +
+      'perspectives.',
     responsibilities: ['Architecture', 'Cybersecurity', 'Cloud Engineering', 'DevOps'],
     technologies: ['Azure', 'Snowflake', 'Terraform'],
     imageLineAwesomeClass: 'la-laptop-medical',
@@ -92,10 +94,10 @@ export const projects: Project[] = [
     allocation: 0.2,
     client: 'Healthcare consultancy',
     description:
-      "PoC for setting up autoscaling for client's Power BI Embedded capacities. " +
+      "PoC for setting up autoscaling for client's Power BI Embedded capacities.\n" +
       'We evaluated whether it was possible to save costs by scaling capacity based on current ' +
-      'usage while maintaining a good user experience. Within a short timeframe, we were able to ' +
-      "discover what options and limitations Power BI Embedded has client's use case.",
+      'usage while maintaining a good user experience. Within a short timeframe, we were able ' +
+      "to discover what options and limitations Power BI Embedded has in client's use case.",
     responsibilities: ['Cost Optimization', 'DevOps'],
     technologies: ['Azure', 'Power BI', 'Terraform'],
     imageLineAwesomeClass: 'la-laptop-medical',
@@ -108,7 +110,7 @@ export const projects: Project[] = [
     client: 'Energy company',
     description:
       'Modernizing a web application and migrating its runtime platform to ' +
-      'Azure Kubernetes Service. Primary goal is to improve maintainability by streamlining ' +
+      'Azure Kubernetes Service.\nPrimary goal is to improve maintainability by streamlining ' +
       "the app's codebase and architecture, and to create a strong foundation for new feature " +
       'development.',
     responsibilities: ['Architecture', 'Full Stack Development'],
